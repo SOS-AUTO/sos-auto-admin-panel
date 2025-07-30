631246ce38f1d7a75ed9d50512e79c56480c4d67
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-07-30 21:03:56 +0100

    feat:update data

--- src/pages/dashboard/error/erro-page.tsx
+++ src/pages/dashboard/error/erro-page.tsx
@@ -1,13 +1,14 @@
 
 import { AlertTriangle } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 interface ErrorPageProps {
   title?: string;
   message?: string;
-  breadcrumb?: string; 
-  onRetry?: () => void; 
-  backTo?: string; 
+  breadcrumb?: string;
+  onRetry?: () => void;
+  backTo?: string;
 }
 
 export default function ErrorPage({
@@ -19,32 +20,32 @@ export default function ErrorPage({
 }: ErrorPageProps) {
 
 
-   return (
-     <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-               {/* Conteúdo principal */}
-        <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)] text-center space-y-4">
-          <AlertTriangle className="w-16 h-16 text-red-500 animate-pulse" />
-          <h1 className="text-2xl font-bold">{title}</h1>
-          <p className="text-gray-500">{message}</p>
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          {/* Conteúdo principal */}
+          <div className="flex flex-col items-center justify-center h-[calc(100vh-4rem)] text-center space-y-4">
+            <AlertTriangle className="w-16 h-16 text-red-500 animate-pulse" />
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <p className="text-gray-500">{message}</p>
 
-          <div className="flex gap-4 mt-4">
-            {onRetry && (
-              <Button variant="default" onClick={onRetry}>
-                Tentar Novamente
-              </Button>
-            )}
-            {backTo && (
-              <Button variant="outline" asChild>
-                <a href={backTo}>Voltar</a>
-              </Button>
-            )}
-          </div>
-        </div>
+            <div className="flex gap-4 mt-4">
+              {onRetry && (
+                <Button variant="default" onClick={onRetry}>
+                  Tentar Novamente
+                </Button>
+              )}
+              {backTo && (
+                <Button variant="outline" asChild>
+                  <Link to={backTo}>Voltar</Link>
+                </Button>
+              )}
             </div>
           </div>
         </div>
-   
+      </div>
+    </div>
+
   );
 }
