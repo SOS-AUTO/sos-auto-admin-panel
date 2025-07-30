e0df6da8d94dfd6a0fa9f0165adc6a9e4a321665
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-07-30 21:04:17 +0100

    feat:update data

--- src/pages/dashboard/error/erro-page.tsx
+++ src/pages/dashboard/error/erro-page.tsx
@@ -18,8 +18,6 @@ export default function ErrorPage({
   onRetry,
   backTo,
 }: ErrorPageProps) {
-
-
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
